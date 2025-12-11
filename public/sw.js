614a9fb318495272cd2b4f3bcc699d7033800671
ilmoinstu <xenima@stunitas.com>
2025-12-11 15:57:31 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'flipcard-v1';
+const urlsToCache = [
+  './',
+  './assets/',
+  './manifest.json',
+  './mdeet_data_utf8.csv'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
---
+++ uploaded_files/tables/(암호화해제)mdeet_parsed.md
@@ -0,0 +1,5 @@
+# (암호화해제)mdeet parsed content
+
+**Note**: Excel parsing may be incomplete; use the original file for complete analysis
+
+|
